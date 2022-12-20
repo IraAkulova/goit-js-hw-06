91306cf2cf6aef208fdb0686e9ea813e5c68cc95
Ira Akulova <ira.akulova2019@gmail.com>
2022-12-20 13:11:18 +0200

    start

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesEl = document.querySelectorAll(".item")
+const countCategories = `Number of categories: ${categoriesEl.length}`;
+
+console.log(countCategories);
+
+
+const itemHeader = categoriesEl.querySelectorAll;
+
+console.log(itemHeader);
