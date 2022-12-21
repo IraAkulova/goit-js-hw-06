daccb88894450b61983e8aa8a986a60b3547c897
Ira Akulova <ira.akulova2019@gmail.com>
2022-12-21 09:21:07 +0200

    done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const allIngredientsEl = document.querySelector("#ingredients")
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.classList = "item";
+  ingredientEl.textContent = ingredient;
+
+  return ingredientEl;
+});
+
+console.log(ingredientsEl);
+
+allIngredientsEl.append(...ingredientsEl);
