9e7159fa22f184108371246c1096ed2ea8e708b3
Ira Akulova <ira.akulova2019@gmail.com>
2022-12-24 12:37:35 +0200

    08 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const refs = {
+    email: document.getElementsByName('email'),
+    password: document.getElementsByName('password'),
+    form: document.querySelector('.login-form'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (event.currentTarget.email.value === '' || event.currentTarget.password.value === '') {
+        return alert('Всі поля форми повинні бути заповнені!');
+    };
+
+    const formEls = event.currentTarget.elements;
+    const email = formEls.email.value;
+    const password = formEls.password.value;
+    const formData = { email, password };
+    console.log(formData);
+
+     event.currentTarget.reset();
+}
