f70d26e33c82523a9bc32543547b80cbb697ea72
Ira Akulova <ira.akulova2019@gmail.com>
2022-12-24 18:57:05 +0200

    09 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+buttonEl.addEventListener('click', onButtonClick);
+function onButtonClick (event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+};
+console.log(spanEl)
