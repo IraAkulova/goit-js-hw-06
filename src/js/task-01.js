d0cd3b391e0f091333ac0243e29e926a162fbc45
Ira Akulova <ira.akulova2019@gmail.com>
2022-12-25 10:34:42 +0200

    01 fixed

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,16 +1,7 @@
-const categoriesEl = document.querySelectorAll(".item");
-const countCategories = `Number of categories: ${categoriesEl.length}`;
-
-console.log(countCategories);
-
-const categoryAndQuantityArray = [...categoriesEl].map(item => {
-    const itemHeader = item.firstElementChild.textContent;
-    const itemMessageEl = `Category: ${itemHeader}`;
-
-    const itemList = item.lastElementChild.children;
-    const countItems = `Elements: ${itemList.length}`;
-
-    return `${itemMessageEl} ${countItems}`;
-})
-const categoryAndQuantity = categoryAndQuantityArray.join(" ");
-console.log(categoryAndQuantity);
+const categories = document.querySelectorAll('.item');
+console.log('Number of categories:', categories.length);
+categories.forEach((element) => {
+    console.log('Category:', element.firstElementChild.textContent);
+    console.log('Elements:', element.lastElementChild.children.length);
+    console.log('');
+});
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector('input'),
+  butCreateEl: document.querySelector('[data-create]'),
+  butDestroyEl: document.querySelector('[data-destroy]'),
+};
+console.log(refs.butDestroyEl)
