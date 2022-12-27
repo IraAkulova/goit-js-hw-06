d5ff392b84014c9e417a45ae9ea360bad8cdac3a
Ira Akulova <ira.akulova2019@gmail.com>
2022-12-27 09:45:04 +0200

    05 fixed mistakes, 10 failed

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,10 +1,10 @@
 const refs = {
-    inpurEl: document.querySelector('#name-input'),
+    inputEl: document.querySelector('#name-input'),
     spanEl: document.querySelector('#name-output'),
 };
 const onInputChange = (event) =>
-    refs.inpurEl.value != '' 
-        ? refs.spanEl.textContent = refs.inpurEl.value
+    refs.inputEl.value != '' 
+        ? refs.spanEl.textContent = refs.inputEl.value
         : refs.spanEl.textContent = "Anonymous";
 
-refs.inpurEl.addEventListener('input', onInputChange);
+refs.inputEl.addEventListener('input', onInputChange);
